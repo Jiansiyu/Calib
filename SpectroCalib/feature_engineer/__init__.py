db454d6726f5ce0eb83023be3b3ee14ff50adb63
Siyu Jian <jiansiyu@gmail.com>
2021-10-01 13:47:59 -0400

    automl works

---
+++ SpectroCalib/feature_engineer/__init__.py
@@ -0,0 +1,9 @@
+from ._preprocess import preprocess
+from ._preprocess import  PolynomialFeatureTransformer
+from ._feature_selection import feature_selection
+
+__all__ = [
+    "preprocess",
+    "PolynomialFeatureTransformer",
+    "feature_selection"
+           ]
--- SpectroCalib/feature_engineer/_feature_selection.py
+++ SpectroCalib/feature_engineer/_feature_selection.py
@@ -5,17 +5,27 @@ Free distribution under MIT license
 
 copyright @ Siyu Jian (sj9va@virginia.edu)
 """
+import pandas
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import mutual_info_regression
+
 class feature_selection(BaseEstimator,TransformerMixin):
     """
-
     """
-    def __init__(self):
+    def __init__(self,model,max_features=20,**params):
+        """
+        :param model:
+        :param max_features:
+        :param params:
+        """
         pass
-
-    def fit(self):
-        return
+    def fit(self,X,y=None):
+        pass
+    def transform(self,X):
+        pass
 
     def fit_transform(self, X, y=None, **fit_params):
-        pass
+        return self.fit(X,y).transform(X)
